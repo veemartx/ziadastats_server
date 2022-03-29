6f5ec80641ad15b7c82f149a5064ed9df8b17898
Vee <vmartxsm@gmail.com>
2022-03-29 12:41:09 +0300

    public home page

--- subprocesses/upload.ps1
+++ subprocesses/upload.ps1
@@ -1,14 +1,14 @@
 # 
-$url="http://localhost:3000/api/db/restore";
+$url="http://139.59.92.126:3000/api/db/restore";
 
-# $file="Z:\NewDBS\AirportNaivas-PhamaDB_FULL_03272022_144035.BAK.zip";
+$file="Z:\NewDBS\AirportNaivas-PhamaDB_FULL_03272022_144035.BAK.zip";
 
 # $file="Z:\NewDBS\LenanaRd-phAMADB_FULL_03272022_111809.BAK.zip";
 
-$file="Z:\NewDBS\AthiRiver-PhamaDB_FULL_03272022_083003.BAK.zip";
+# $file="Z:\NewDBS\AthiRiver-PhamaDB_FULL_03272022_083003.BAK.zip";
 
 $Form = @{
-    branch      = 'AthiRiver'
+    branch      = 'AirportNaivas'
     backup     = Get-Item -Path $file
 }
 
